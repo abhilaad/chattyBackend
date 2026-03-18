06704c41c3596a3a0d50b59aef13007ac76f43be
abhilaad <abhishek.24042013@gmail.com>
2026-03-19 01:38:12 +0530

    koyeb port issue temp

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -22,7 +22,7 @@ import { SocketIONotificationHandler } from '@socket/notification';
 import { SocketIOImageHandler } from '@socket/image';
 import { SocketIOChatHandler } from '@socket/chat';
 
-const SERVER_PORT = 5000;
+const SERVER_PORT = 8000;
 const log: Logger = config.createLogger('server');
 
 export class ChattyServer {
